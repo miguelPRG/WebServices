b143e5961122598c36cd50bb3caaafb8cc9c8f48
miguelgoncalves2024 <miguelgoncalves2024@hotmail.com>
2026-02-27 14:38:03 +0000

    First

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+# Para executar a aplicação, use o comando: uvicorn main:app --reload
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
